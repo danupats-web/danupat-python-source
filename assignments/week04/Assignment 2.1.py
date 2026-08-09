553668b39a037b2249e469e8214cbb0b02776f66
Danupat Srisuktasin <danupat.s@ku.th>
2026-08-09 13:18:35 +0700

    assignment 2.1,2.2

---
+++ assignments/week04/Assignment 2.1.py
@@ -0,0 +1,14 @@
+scores = []
+
+for i in range(5):
+    score = int(input(f"Enter score of student {i + 1}: "))
+    scores.append(score)
+
+print()
+
+for i in range(5):
+    if scores[i] >= 50:
+        result = "ผ่าน"
+    else:
+        result = "ไม่ผ่าน"
+    print(f"Student {i + 1}: {scores[i]} --> {result}")
